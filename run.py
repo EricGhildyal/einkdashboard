f40d860c58a7bc0d9553f5d6224c0cf1bbfbf580
Eric Ghildyal <ericghildyal@gmail.com>
2020-11-11 01:37:43 +0000

    Increase font size

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
 import os
 import pickle
 import time
@@ -10,6 +14,9 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from waveshare_epd import epd7in5_V2
 from PIL import Image, ImageDraw, ImageFont
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 UPTIMEROBOT_API_URL = "https://api.uptimerobot.com/v2"
 SENTRY_API_URL = "https://sentry.io/api/0"
@@ -24,7 +31,7 @@ def get_down_monitors():
                         })
     if res.status_code == 200 and res.json()['stat'] == "ok":
         return [monitor['friendly_name'] for monitor in res.json()['monitors']]
-    print(f"Error getting down monitors: {res.json()}")
+    logger.error(f"Error getting down monitors: {res.json()}")
     return None
 
 def get_service_ratios():
@@ -36,6 +43,7 @@ def get_service_ratios():
                         })
     if res.status_code == 200 and res.json()['stat'] == "ok":
         for monitor in res.json()['monitors']: # we want an avg over all the services on different clusters
+            logger.info(monitor['friendly_name'].split(" ")[0])
             serv_name = monitor['friendly_name'].split(" ")[0]
             if serv_name in ratios:
                 ratios[serv_name].append(float(monitor['custom_uptime_ratio']))
@@ -45,7 +53,7 @@ def get_service_ratios():
             if isinstance(ratios[ratio], list):
                 ratios[ratio] = round(sum(ratios[ratio]) / len(ratios[ratio]), 2)
         return ratios
-    print(f"Error getting service ratios: {res.json()}")
+    logger.error(f"Error getting service ratios: {res.json()}")
     return None
 
 def get_sentry_events(project):
@@ -62,7 +70,7 @@ def get_sentry_events(project):
                 "count": event['count'] 
             })
         return out
-    print(f"Error getting sentry events: {res.json()}")
+    logger.error(f"Error getting sentry events: {res.json()}")
     return None
 
 def get_cal_events(num):
@@ -75,13 +83,14 @@ def get_cal_events(num):
 
 def display(epd, uptimes, down, backend_events, frontend_events):
     try:
-        print("Displaying...")
+        logger.info("Drawing new data")
         display_font = ImageFont.truetype('Righteous-Regular.ttf', 38)
         display_font_sm = ImageFont.truetype('Righteous-Regular.ttf', 20)
-        display_font_xs = ImageFont.truetype('Righteous-Regular.ttf', 15)
+        display_font_xs = ImageFont.truetype('Righteous-Regular.ttf', 17)
         number_font = ImageFont.truetype('KellySlab-Regular.ttf', 25)
         image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(image)
+        logger.info("Drawing Uptimes")
         # uptimes
         draw.text((5, 0), 'Uptime', font = display_font, fill = 0)
         x = 10
@@ -93,6 +102,7 @@ def display(epd, uptimes, down, backend_events, frontend_events):
             y += 26
         draw.line((0, 50, 150, 50), fill = 0, width = 3)
         draw.line((150, 0, 150, epd.height), fill = 0, width = 3)
+        logger.info("Drawing Down monitors")
         # down monitors, if none down, just don't show this section
         if len(down) > 0:
             draw.text((160, 0), '!! Down Monitors !!', font = display_font, fill = 0)
@@ -103,6 +113,7 @@ def display(epd, uptimes, down, backend_events, frontend_events):
         else:
             x = 160
             y = 0
+        logger.info("Drawing backend events")
         # backend events
         draw.text((x, y), 'Backend', font = display_font, fill = 0)
         draw.line((x-10, y+50, epd.width, y+50), fill = 0, width = 3)
@@ -112,6 +123,7 @@ def display(epd, uptimes, down, backend_events, frontend_events):
             y += 18
             draw.text((x+12, y), event['culprit'], font = display_font_xs, fill = 0)
             y += 20
+        logger.info("Drawing frontend events")
         # frontend events
         draw.text((x, y), 'Frontend', font = display_font, fill = 0)
         draw.line((x-10, y+50, epd.width, y+50), fill = 0, width = 3)
@@ -121,34 +133,36 @@ def display(epd, uptimes, down, backend_events, frontend_events):
             y += 18
             draw.text((x+12, y), event['culprit'], font = display_font_xs, fill = 0)
             y += 20
+        logger.info("Displaying...")
         epd.display(epd.getbuffer(image))
     except IOError as err:
-        print(f"Error: {err}")
+        logger.error(f"Error: {err}")
     except KeyboardInterrupt:
-        print("^C")
+        logger.error("^C")
         epd7in5_V2.epdconfig.module_exit()
         exit()
 
 def main():
+    logger.info("Started")
     epd = epd7in5_V2.EPD()
     epd.init()
     epd.Clear()
     old_data = -1
     new_data = 1
     while True:
-        print("Fetching new data...")
+        logger.info("Fetching new data...")
         uptimes = get_service_ratios()
         down = get_down_monitors()
         frontend_events = get_sentry_events("frontend")
         backend_events = get_sentry_events("backend")
         new_data = hash(str(uptimes) + str(down) + str(frontend_events) + str(backend_events))
         if new_data != old_data:
-            print("displaying new data")
+            logger.info("displaying new data")
             display(epd, uptimes, down, backend_events, frontend_events)
         else:
-            print("data hasn't changed")
+            logger.info("data hasn't changed")
         old_data = new_data
-        print("sleeping...")
+        logger.info("sleeping...")
         time.sleep(30)
 
 if __name__ == "__main__":
